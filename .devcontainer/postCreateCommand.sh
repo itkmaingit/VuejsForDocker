06cb81ae1b6ac0d63e2de4171e6077757b1ff4dc
itkmaingit <itkmain.git@gmail.com>
2023-02-06 18:39:50 +0900

    完成版

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+yes |  npx nuxi init /home/node/application
+cd /home/node/application
+npm install yarn &&  yarn add @mdi/font mdi sass vuetify@next webfontloader &&  yarn install &&  mkdir pages components plugins
+sudo cp /home/node/copy_modules/vuetify.js /home/node/copy_modules/webfontloader.client.js /home/node/application/plugins/
+sudo cp /home/node/copy_modules/nuxt.config.ts /home/node/application/
+sudo rm -r /home/node/copy_modules
