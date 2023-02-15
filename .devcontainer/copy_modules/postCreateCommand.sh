2a67d6da3f214478462055217d1c2b3caa1a6a2d
Dango <68278041+itkmaingit@users.noreply.github.com>
2023-02-15 14:06:56 +0900

    modify .prettierrc.js

--- .devcontainer/copy_modules/postCreateCommand.sh
+++ .devcontainer/copy_modules/postCreateCommand.sh
@@ -4,5 +4,5 @@ yes |  npx nuxi init /home/node/application
 cd /home/node/application
 npm install yarn &&  yarn add @mdi/font mdi sass vuetify@next webfontloader &&  yarn install &&  mkdir pages components plugins
 sudo cp /home/node/copy_modules/vuetify.js /home/node/copy_modules/webfontloader.client.js /home/node/application/plugins/
-sudo cp /home/node/copy_modules/nuxt.config.ts /home/node/copy_modules/.eslintrc.json /home/node/copy_modules/prettierrc.js /home/node/application/
+sudo cp /home/node/copy_modules/nuxt.config.ts /home/node/copy_modules/.eslintrc.json /home/node/copy_modules/.prettierrc.js /home/node/application/
 sudo rm -r /home/node/copy_modules
